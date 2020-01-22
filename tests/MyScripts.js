37d34cfce337b2e54ebef4a99815cb7ae6de34e0
BrianGodinez <55292738+BrianGodinez@users.noreply.github.com>
2020-01-22 13:53:42 -0500

    Update MyScripts.js

--- tests/MyScripts.js
+++ tests/MyScripts.js
@@ -1,24 +1,9 @@
-document.write("1.For Loop <br  />");
-for(var mod = 0; mod<1; mod++){
-  for (var i = 1; i < 10; i++)
- document.writeln(i+"<br>");
+function myLoop(){
+var start= document.getElementById("start").value;
+var end = document.getElementById("end").value;
+console.log(start);
+for(var i = start; i <= end; i++)
+  {
+    document.write('<p>' + i + '</p>');
+  }
 }
-
-var start = 0;
-var end = 10;
-
-
-document.write("2.While Loop <br  />");
-
-while(start < end)
-{
-document.write("Loop: " + start +"<br />");
-  start++;
-}
-document.write("3.Math.sqrt  <br  />");
-  document.write(Math.sqrt(4) + "<br />");  
-    document.write(Math.sqrt(25) + "<br />");  
-
-document.write("4.Math.random  <br  />");
-var random = Math.random( ); 
-    document.write("Random Number Generated : " + random ); 
