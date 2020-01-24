1107967b8fb9c72541d3b3f0275a0d3a7b7edeb3
BrianGodinez <55292738+BrianGodinez@users.noreply.github.com>
2020-01-24 13:29:11 -0500

    Update MyScripts.js

--- tests/MyScripts.js
+++ tests/MyScripts.js
@@ -1,8 +1,17 @@
 function myLoop(){
 var start= document.getElementById("start").value;
 var end = document.getElementById("end").value;
-console.log(start);
-for(var i = parseInt (start); i <= end; i++)
+var jump = document.getElementById("jump").value;
+  
+console.log(typeof start);
+  console.log(typeof end);
+  console.log(typeof jump);
+  
+  start = Number(start);
+   end = Number(end);
+  jump = Number(jump);
+  
+for(var i = start; i <= end; i+=jump)
   {
     document.write('<p>' + i + '</p>');
   }
