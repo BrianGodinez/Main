cdd24d122a42321d6cf440fb04fa178d8a045597
BrianGodinez <55292738+BrianGodinez@users.noreply.github.com>
2020-01-24 12:46:16 -0500

    Update MyScripts.js

--- tests/MyScripts.js
+++ tests/MyScripts.js
@@ -2,7 +2,7 @@ function myLoop(){
 var start= document.getElementById("start").value;
 var end = document.getElementById("end").value;
 console.log(start);
-for(var i = start; i <= end; i++)
+for(var i = parseInt (start); i <= end; i++)
   {
     document.write('<p>' + i + '</p>');
   }
